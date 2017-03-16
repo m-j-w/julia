af710988269e3b3523e31288de74459c30c8d910
Stefan Karpinski <stefan@karpinski.org>
2017-03-16 13:57:59 -0400

    add docstring for Core.TypeofBottom

--- base/docs/basedocs.jl
+++ base/docs/basedocs.jl
@@ -675,6 +675,14 @@ generation specialization for that field.
 ANY
 
 """
+    Core.TypeofBottom
+    Type{Union{}}
+
+The singleton type containing only the value `Union{}`.
+"""
+Core.TypeofBottom
+
+"""
     DevNull
 
 Used in a stream redirect to discard all data written to it. Essentially equivalent to
